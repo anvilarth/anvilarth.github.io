bfe69f1eb8ec8bbfbb39385f4080cafdda5e5107
anvilarth <filatovandreiv@gmail.com>
2023-06-11 21:08:31 +0300

    upd

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://anvilarth.github.io',
-  title: 'JS.',
+  title: '',
 }
 
 const about = {
@@ -9,11 +9,11 @@ const about = {
   name: 'Andrei Filatov',
   role: 'Research scientist',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'I have over 4 years of experience in ML/DL gained through my work with EPFL and Samsung. I have experience working in various fields such as Computer Vision, NLP and Meta-learning. Currently, my focus is on the application of language models to different types of data. Additionally, I work as a lecturer for a Deep Learning course. I am highly motivated and interested in the fields of multi-task learning and multi-modal learning. ',
+  resume: 'https://drive.google.com/file/d/1_yudui9m5TRWGtD9XvKc7eTBAn0hblvv/view?usp=sharing',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/andrei-filatov',
+    github: 'https://github.com/anvilarth',
   },
 }
 
@@ -21,51 +21,36 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Task Discovery (NeurIPS 2022)',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Learn how we discover tasks on which a NNs generalize well by optimizing the agreement score objective.',
+    stack: ['PyTorch', 'Computer Vision', 'Meta-Learning'],
+    livePreview: 'taskdiscovery.epfl.ch',
   },
   {
-    name: 'Project 2',
+    name: 'Deep Learning course',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
+      'In a team of two people prepared and conducted course on Deep Learning. The course covers: basics of neural networks, sequence processing, computer vision, reinforcement learning, generative models',
+      stack: [],
+      livePreview: 'https://github.com/intsystems/Deep-Learning-Course-2022',
+  }
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Python',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'PyTorch',
+  'Jax',
+  'Numpy',
+  'Pandas',
+  'SQL',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'filatovandreiv@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
